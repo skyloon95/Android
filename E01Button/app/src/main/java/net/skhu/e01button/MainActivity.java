6bb31ee63ded88c97a0c8804913fc89f7a469a5b
모설아 <skyloon95@gmail.com>
2018-09-12 10:41:02 +0900

    E02views 예제. 메뉴만들기. always never ifroom 조건 등

--- E01Button/app/src/main/java/net/skhu/e01button/MainActivity.java
+++ E01Button/app/src/main/java/net/skhu/e01button/MainActivity.java
@@ -2,6 +2,7 @@ package net.skhu.e01button;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,21 +10,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "내로그";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate 메소드 시작");
 
         Button b = (Button)findViewById(R.id.button);
         View.OnClickListener listenerObj = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick 메소드 시작");
                 EditText e = (EditText)findViewById(R.id.editText);
                 CharSequence s = e.getText();
+                Log.d(TAG, "EditText에 입력된 문자열 :" + s);
                 TextView t = (TextView)findViewById(R.id.textView);
                 t.setText(s);
+                Log.d(TAG, "onClick 메소드 종료");
             }
         };
         b.setOnClickListener(listenerObj);
+
+        Log.d(TAG, "onCreate 메소드 종료");
     }
 }
---
+++ E02views/app/src/main/java/net/skhu/e02views/MainActivity.java
@@ -0,0 +1,34 @@
+package net.skhu.e02views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //  화면 구성요소 내용을 화면에 불러와 채운다. ( 레이아웃에 따라 ) = layout inflation
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_signUp) {
+            Toast.makeText(this, "회원가입 메뉴 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_memo) {
+            Toast.makeText(this, "메모장 메뉴 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
